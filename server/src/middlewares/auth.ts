2ee40abd628501b910c9927a3e991630f98a9e01
claudiodantas <claudiodantas1996@gmail.com>
2023-04-20 10:00:37 -0300

    feat: add middleware to validate jwt

---
+++ server/src/middlewares/auth.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express"
+import jwt from 'jsonwebtoken'
+import { User } from "../models/User"
+
+interface JwtPayload {
+    id: number | string
+}
+
+export const isAuth = async (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers
+    if (!authorization) return res.status(401).send({ message: "Usuário não autorizado." })
+
+    const token = authorization.split(' ')[1]
+
+    const { id } = jwt.verify(token, 'anicetokenkey') as JwtPayload
+
+    const user = await User.findById({ _id: id })
+
+    if (!user) return res.status(401).send({ message: "Usuário encontrado." })
+
+    const loggedUser = {
+        name: user.fullname,
+        email: user.email
+    }
+
+    req.user = loggedUser
+
+    next()
+}
