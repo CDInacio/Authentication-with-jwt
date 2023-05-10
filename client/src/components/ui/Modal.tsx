6ec9841be53c6905ac0e8adc7daa2bbfba2838c2
Cláudio Vitor Dantas <claudiodantas1996@gmail.com>
2023-05-10 14:10:14 -0300

    Update Modal.tsx

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -13,35 +13,33 @@ const Modal = () => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-blackA data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-          <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-            Edit profile
+        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-dark p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <Dialog.Title className="text-white m-0 text-[17px] font-medium">
+            Adicionar tarefa
           </Dialog.Title>
-          <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
+          {/* <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
             Make changes to your profile here. Click save when you're done.
-          </Dialog.Description>
-          <fieldset className="mb-[15px] flex items-center gap-5">
-            <label
-              className="text-violet11 w-[90px] text-right text-[15px]"
-              htmlFor="name"
-            >
-              Name
+          </Dialog.Description> */}
+          <fieldset className="mb-[15px] flex flex-col gap-5">
+            <label className="text-white w-[90px]  text-[15px]" htmlFor="name">
+              Título
             </label>
             <input
-              className="text-violet11 shadow-violet7 focus:shadow-purlpe inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+              className="text-primary-500 duration-300 bg-dark shadow-neutral-500 focus:shadow-purlpe inline-flex py-[10px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
               id="name"
               defaultValue="Pedro Duarte"
             />
           </fieldset>
-          <fieldset className="mb-[15px] flex items-center gap-5">
+          <fieldset className="mb-[15px] flex flex-col  gap-5">
             <label
-              className="text-violet11 w-[90px] text-right text-[15px]"
+              className="text-white w-[90px]   text-[15px]"
               htmlFor="username"
             >
-              Username
+              Descrição
             </label>
-            <input
-              className="text-violet11 shadow-violet7 focus:shadow-purlpe inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+            <textarea
+              rows={10}
+              className="text-primary-500 duration-300 bg-dark shadow-neutral-500 focus:shadow-purlpe inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
               id="username"
               defaultValue="@peduarte"
             />
@@ -49,7 +47,7 @@ const Modal = () => {
           <div className="mt-[25px] flex justify-end">
             <Dialog.Close asChild>
               <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
-                Save changes
+                Salvar
               </button>
             </Dialog.Close>
           </div>
